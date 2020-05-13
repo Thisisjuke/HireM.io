41a1f31c0886f172586125e1b739048dd9d8367e
theosenoussaoui <theosen95@gmail.com>
2020-05-13 23:56:41 +0200

    removing useless bottombar styles

--- src/components/BottomBarNavigator/styles.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
-
-const Tab = createMaterialBottomTabNavigator();
-
-export const StyledTab = styled(Tab)`
-  background-color: red !important;
-`;
